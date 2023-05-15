851a284613afee93849d7a0938d68573c35313c8
nugo128 <nugo.rostiashvili@redberry.ge>
2023-05-15 15:23:59 +0400

    style: starting text appear animation

--- src/pages/StartingPage.vue
+++ src/pages/StartingPage.vue
@@ -3,6 +3,17 @@
     <transition name="logo" @after-enter="imgEnter" appear>
       <img class="w-24 h-24" src="../assets/images/StartLogo.png" alt="logo" />
     </transition>
+    <div v-if="imageIsSet" class="w-96 h-24 bg-[#B5C0C7] z-50"></div>
+    <transition name="link" appear>
+      <div
+        v-if="imageIsSet"
+        class="z-0 font-bold text-3xl w-24 flex justify-center"
+      >
+        <router-link :to="'personal-info'" class="text-center"
+          >კითხვარის დაწყება</router-link
+        >
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -31,5 +42,15 @@ export default {
 .logo-enter-to {
   translate: 0px -85px;
   scale: 1;
+}
+.link-enter-from {
+  translate: 0px -110px;
+  opacity: 0;
+}
+.link-enter-active {
+  transition: all 1s ease-out;
+}
+.link-enter-to {
+  opacity: 1;
 }
 </style>
