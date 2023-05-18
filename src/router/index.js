1da70dec8353172472f609251f9064e6d210fba5
nugo128 <nugo.rostiashvili@redberry.ge>
2023-05-18 17:35:19 +0400

    feat: add covid questions page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import StartingPage from "../pages/StartingPage.vue";
 import PersonalInfo from "../pages/PersonalInfo.vue";
-
+import CovidQuestions from "../pages/CovidQuestions.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -14,6 +14,11 @@ const router = createRouter({
       path: "/personal-info",
       name: "PersonalInformation",
       component: PersonalInfo,
+    },
+    {
+      path: "/covid-questions",
+      name: "CovidQuestions",
+      component: CovidQuestions,
     },
   ],
 });
