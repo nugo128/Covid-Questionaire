cbe60c79b0ff0463bf1895c8575e26209446d16f
nugo128 <nugo.rostiashvili@redberry.ge>
2023-05-18 17:39:18 +0400

    feat: save data to vuex and localstorage

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,1 +1,18 @@
-export default {};
+export default {
+  saveData(state, data) {
+    state.info.name = data;
+    console.log(state);
+  },
+  saveLastname(state, lastname) {
+    state.info.lastname = lastname;
+    console.log(state);
+  },
+  saveEmail(state, email) {
+    state.info.email = email;
+    console.log(state);
+  },
+  saveDate(state, date) {
+    state.info.period = date;
+    console.log(state);
+  },
+};
